20533d943539c2e7c9ed8d92969f876b0085d6c1
TonyHoh <50274524+TonyHoh@users.noreply.github.com>
2023-11-02 00:56:03 +0100

    Spremanje untracked datoteka

---
+++ IzvorniKod/CookBooked-backend/src/main/java/opp/CookBookedBackendApplication.java
@@ -0,0 +1,13 @@
+package opp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookBookedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookBookedBackendApplication.class, args);
+	}
+
+}
