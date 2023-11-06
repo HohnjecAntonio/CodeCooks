cca7a15afacce82054f0f1cf594346c409198b2d
Armis Kantarević <ak53739@fer.hr>
2023-11-06 23:34:04 +0100

    Dodavanje mogućnosti za manipuliranje objektom korisnik
    Dodavanje u interface: dohvat korisnika po ID, pronalaženje po ID i username-u, te brisanje korisnika.

--- IzvorniKod/CookBooked-backend/src/main/java/opp/service/KorisnikService.java
+++ IzvorniKod/CookBooked-backend/src/main/java/opp/service/KorisnikService.java
@@ -6,5 +6,13 @@ import java.util.List;
 
 public interface KorisnikService {
     List<Korisnik> listAll();
+
+    Korisnik fetch(Long iDKorisnik);
     Korisnik createKorisnik(Korisnik korisnik);
+
+    Optional<Korisnik> findById(Long iDKorisnik);
+
+    Optional<Korisnik> findByUsername(String korisnickoIme);
+
+    Korisnik deleteKorisnik(Long iDKorisnik);
 }
