168b04ac09ab997e11860c75361e93645a3208f3
Antonio Hohnjec <antonio.hohnjec@fer.hr>
2023-11-15 03:14:34 +0100

    fix: izmjena proxya i dodavanje koda kako bi se pokusalo uspostaviti veza na renderu

--- IzvorniKod/cookbookedfrontend/app.js
+++ IzvorniKod/cookbookedfrontend/app.js
@@ -21,10 +21,6 @@ app.use(
 
 app.use(express.static(path.join(__dirname, 'build')))
 
-app.listen(PORT, HOST, () => {
-    console.log(`Starting Proxy at ${HOST}:${PORT}`);
-});
-
 app.get("*", async (req, res) => {
         res.sendFile(path.join(__dirname, 'build', 'index.html'))
     }
