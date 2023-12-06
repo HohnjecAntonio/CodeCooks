4786b5d00fbace797d8cac35f84a3fa6dad9bdb1
Armis Kantarević <ak53739@fer.hr>
2023-12-06 15:16:31 +0100

    Ubacivanje funkcionalnosti za promjenu podataka korisnikovog profila

--- IzvorniKod/CookBooked-backend/src/main/java/opp/controller/KorisnikController.java
+++ IzvorniKod/CookBooked-backend/src/main/java/opp/controller/KorisnikController.java
@@ -1,8 +1,10 @@
 package opp.controller;
 
 import opp.model.Korisnik;
+import opp.service.EntityMissingException;
 import opp.service.KorisnikService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -30,5 +32,31 @@ public class KorisnikController {
     public Korisnik createKorisnik(@RequestBody Korisnik korisnik){
         Korisnik saved = korisnikService.createKorisnik(korisnik);
         return ResponseEntity.created(URI.create("/korisnici/" + saved.getIDKorisnik())).body(saved).getBody();
+    }
+
+    @PutMapping("/{korisnickoIme}")
+    public ResponseEntity<Korisnik> updateKorisnik(@PathVariable("korisnickoIme") String korisnickoIme, @RequestBody Korisnik updatedKorisnik) {
+        Korisnik existingKorisnik = korisnikService.fetch(korisnickoIme);
+
+        existingKorisnik.setLozinkaKorisnik(updatedKorisnik.getLozinkaKorisnik());
+        existingKorisnik.setImeKorisnik(updatedKorisnik.getImeKorisnik());
+        existingKorisnik.setPrezimeKorisnik(updatedKorisnik.getPrezimeKorisnik());
+        existingKorisnik.setBrojTelefona(updatedKorisnik.getBrojTelefona());
+        existingKorisnik.setEmailKorisnik(updatedKorisnik.getEmailKorisnik());
+
+        Korisnik updated = korisnikService.updateKorisnik(existingKorisnik);
+
+        return ResponseEntity.ok(updated);
+    }
+
+    @DeleteMapping("/{korisnickoIme}")
+    public ResponseEntity<String> deleteKorisnik(@PathVariable("korisnickoIme") String korisnickoIme) {
+        try {
+            Korisnik deletedKorisnik = korisnikService.deleteKorisnik(korisnickoIme);
+            return ResponseEntity.ok("Account with username " + korisnickoIme + " has been successfully deleted.");
+        } catch (EntityMissingException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Account with username " + korisnickoIme + " not found.");
+        }
     }
 }
