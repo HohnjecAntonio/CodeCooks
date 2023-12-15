1f85422b1c1b42f5629a78136548388899d2af4b
MarkoPavic54378 <148389159+MarkoPavic54378@users.noreply.github.com>
2023-12-15 17:34:18 +0100

    Update App.js - dodana funkcija toggleSidebar - za expand i collapse sidebar-a

--- IzvorniKod/cookbookedfrontend/src/App.js
+++ IzvorniKod/cookbookedfrontend/src/App.js
@@ -1,3 +1,4 @@
+import React , {useState}from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Registracija from "./komponente/Registracija";
@@ -7,15 +8,29 @@ import NoPage from "./komponente/NoPage";
 import Atributions from "./komponente/Atributions";
 import SideBar from "./komponente/SideBar";
 
-
 function App() {
+    
+  const [isSidebarOpen, setSidebarOpen] = useState(true);
 
-  return (
+  const toggleSidebar = () => {
+   
+    setSidebarOpen(!isSidebarOpen);
+
+    const sidebar = document.querySelector('.sidebar');
+
+    if (isSidebarOpen) {
+      sidebar.classList.add('open');
+    } else {
+      sidebar.classList.remove('open');
+    }
+  };
+    return (
       <div className="App">
           <div className="Header">
-              <div className="MenuIkona">
-                  <button></button>
-              </div>
+            <div className="MenuIkona">
+                <button onClick={toggleSidebar}></button>
+            </div>
+
               <div className="Ikona">
                   <h4 className = "ContainsIkona">CookBooked</h4>
               </div>
@@ -33,7 +48,13 @@ function App() {
               </a>
           </div>
             <div className="Tijelo">
-
+            
+                    <div className={`sidebar ${isSidebarOpen ? 'open' : ''}`}>
+                    <SideBar/>
+                    
+                    </div>
+                    
+               
               <BrowserRouter>
                   <Switch>
                       <Route path="/" exact>{<Home/>}</Route>
@@ -44,7 +65,7 @@ function App() {
                   </Switch>
               </BrowserRouter>
 
-                <SideBar/>
+                
             </div>
       </div>
   );
