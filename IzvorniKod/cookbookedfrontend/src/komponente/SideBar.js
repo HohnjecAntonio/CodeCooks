1b04176ad0f1696a511506afce3f3eb8298eb476
MarkoPavic54378 <148389159+MarkoPavic54378@users.noreply.github.com>
2023-12-15 17:32:08 +0100

    Update SideBar.js- update za primanje kategorija iz backenda i prikazivanja njih u sidebar-u u obliku gumba

--- IzvorniKod/cookbookedfrontend/src/komponente/SideBar.js
+++ IzvorniKod/cookbookedfrontend/src/komponente/SideBar.js
@@ -1,21 +1,62 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./SideBar.css"
+import "./CategoryButtons.js"
 
 function SideBar(){
+    
+
+    const [categories, setCategories] = useState([]);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        // Replace 'YOUR_API_URL' with the actual API endpoint that provides categories
+        const apiUrl = 'YOUR_API_URL';
+
+        fetch(apiUrl)
+        .then((response) => {
+            if (!response.ok) {
+            throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then((data) => setCategories(data))
+        .catch((err) => setError(err.message));
+    }, []);
+    /*const sampleCategories = [
+        { id: 1, name: 'Category 1' },
+        { id: 2, name: 'Category 2' },
+        { id: 3, name: 'Category 3' },
+        
+    ];
+    
+    const [categories] = useState(sampleCategories);*/
     return (
         <div className="Meni">
             <ul className="KategorijeJela">
-                <h4>Kategorije jela:</h4>
-                <li><a>Kategorija primjer 1</a></li>
-                <li><a>Kategorija primjer 2</a></li>
+            <div>
+                <div>
+                <h2 style={{ fontSize: '40px', border: '10px', borderColor: '#000000' ,marginBottom:'30px'}}>KategorijeJela</h2>
+                {error && <p>Error: {error}</p>}
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center',gap: '0px' }}>
+                    {categories.map((category) => (
+                    <button key={category.id} className="buttonconfi">
+                        {category.name}
+                    </button>
+                    ))}
+                </div>
+                </div>
+              </div>
             </ul>
+            
             <ul className="RegijaJela">
-                <h4>Regije jela:</h4>
-                <li><a>Regija primjer 1</a></li>
-                <li><a>Regija primjer 2</a></li>
+                <h4 style={{ fontSize: '40px', border: '10px', borderColor: '#000000' ,marginBottom:'30px'}}>Regije jela:</h4>
+                <li ><p>Regija primjer 1</p></li>
+                <li ><p>Regija primjer 2</p></li>
             </ul>
         </div>
     )
+
+
 }
 
 export default SideBar;
