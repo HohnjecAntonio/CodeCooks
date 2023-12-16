a13821adb92490fd34858be50d256cb68f1590c4
purham01 <benjamin.zrakic@fer.hr>
2023-12-16 17:21:29 +0100

    Par css fixeva

--- IzvorniKod/cookbookedfrontend/src/App.js
+++ IzvorniKod/cookbookedfrontend/src/App.js
@@ -56,8 +56,9 @@ function App() {
                   </Switch>
               </BrowserRouter>
 
-                <SideBar/>
+                
             </div>
+            <SideBar/>
       </div>
   );
 }
