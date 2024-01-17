7f170b0346c1c42fddf341c91c4639ac946831ef
MarkoPavic54378 <148389159+MarkoPavic54378@users.noreply.github.com>
2024-01-17 15:22:33 +0100

    update za dev-active

---
+++ IzvorniKod/HomePage.jsx
@@ -0,0 +1,133 @@
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchKategorije } from '../../redux/auth/auth.action.js'; // Import the action
+import './HomePage.css'; // You can create a separate CSS file for styling
+import {fetchRecipesForUserFeed} from "../../redux/auth/auth.action";
+
+
+const HomePage = () => {
+    const dispatch = useDispatch();
+    const kategorije = useSelector(state => state.auth.kategorije); 
+    const loading = useSelector(state => state.auth.loading);
+    const error = useSelector(state => state.auth.error);
+    const recipesForFeed = useSelector(state => state.auth.recipesForFeed); 
+
+    useEffect(() => {
+        dispatch(fetchRecipesForUserFeed());
+    }, [dispatch]);
+
+    useEffect(() => {
+        dispatch(fetchKategorije());
+    }, [dispatch]);
+
+    const [recipes, setRecipes] = useState([
+      {
+        id: 1,
+        title: 'Spaghetti Bolognese',
+        ingredients: ['500g spaghetti', '400g minced beef', '1 onion', '2 cloves garlic', '400g tomato sauce'],
+        instructions: 'Cook spaghetti according to package instructions. In a pan, sauté onions and garlic, add minced beef, cook until browned, then add tomato sauce. Serve over cooked spaghetti.',
+        category: 'Pasta',
+        image: 'images/spaghetti.jpg',
+        creator: 'Chef John Doe',
+        userID: '1'
+      },
+      {
+        id: 2,
+        title: 'Chicken Stir Fry',
+        ingredients: ['400g chicken breast', '1 bell pepper', '1 broccoli', '2 tbsp soy sauce'],
+        instructions: 'Slice chicken and vegetables. Stir-fry chicken until cooked, add vegetables and soy sauce. Cook until veggies are tender. Serve hot.',
+        category: 'Asian',
+        image: 'images/stir_fry.jpg',
+        creator: 'Chef Jane Smith',
+        userID: '1'
+      },
+      {
+        id: 3,
+        title: 'Chicken Stir Fry',
+        ingredients: ['400g chicken breast', '1 bell pepper', '1 broccoli', '2 tbsp soy sauce'],
+        instructions: 'Slice chicken and vegetables. Stir-fry chicken until cooked, add vegetables and soy sauce. Cook until veggies are tender. Serve hot.',
+        category: 'Asian',
+        image: 'images/stir_fry.jpg',
+        creator: 'New user',
+        userID: '2'
+      },
+      {
+        id: 4,
+        title: 'Chicken Stir Fry',
+        ingredients: ['400g chicken breast', '1 bell pepper', '1 broccoli', '2 tbsp soy sauce'],
+        instructions: 'Slice chicken and vegetables. Stir-fry chicken until cooked, add vegetables and soy sauce. Cook until veggies are tender. Serve hot.',
+        category: 'Asian',
+        image: 'images/stir_fry.jpg',
+        creator: 'New user',
+        userID: '2'
+      },
+      {
+          id: 5,
+          title: 'Chicken Stir Fry',
+          ingredients: ['400g chicken breast', '1 bell pepper', '1 broccoli', '2 tbsp soy sauce'],
+          instructions: 'Slice chicken and vegetables. Stir-fry chicken until cooked, add vegetables and soy sauce. Cook until veggies are tender. Serve hot.',
+          category: 'Asian',
+          image: 'images/stir_fry.jpg',
+          creator: 'New user',
+          userID: '2'
+        },
+      // Add more recipe objects as needed
+    ]);
+  
+  
+    const [newRecipe, setNewRecipe] = useState({
+      id: '',
+      title: '',
+      ingredients: '',
+      instructions: '',
+      category: '',
+      creator: '',
+      userID: ''
+    });
+
+    return (
+        <div className='px-20'>
+            <h1>Dobro došli na CookBooked!</h1>
+            {loading && <p>Loading...</p>}
+            {error && <p>Error: {error.message}</p>}
+            {/*<h3 className='text-4xl mt-5 mb-3'>Kategorije</h3>
+            <div>
+                {kategorije.map(kategorija => (
+                    <div key={kategorija.idKategorija}>
+                        {kategorija.nazivKategorija}
+                    </div>
+                ))}
+                </div>*/}
+            <div className="recipe-page">
+            <h1 style={{ fontSize: '75px' , fontWeight: 'bold'}}>CookBooked</h1>
+            <h1 style={{ fontSize: '50px' }}>Recipes</h1>
+          <div className="recipe-list">
+            
+            {recipesForFeed.map((recipe) => (
+              <div key={recipe.idRecept} className="recipe-card">
+
+                <a href="/RecipePage" style={{ fontWeight: 'bold' , fontSize: '20px', color: '#000'}} onClick={()=>{localStorage.setItem('recipeToLoad',JSON.stringify(recipe.idRecept)); console.log(recipe.idRecept);}}>
+                <div className="recipe-details">
+                  <h2>{recipe.nazivRecept}</h2>
+                  {/*<p>Category: {recipe.category}</p>
+                  <ul>
+                    {recipe.ingredients.map((ingredient, index) => (
+                      <li key={index}>{ingredient}</li>
+                    ))}
+                  </ul>*/}
+                  <p>Uputstva: {recipe.priprema}</p>
+                  <p>Autor: <a href="/Profile" onClick={() => {
+                      localStorage.setItem('profileToLoad',JSON.stringify(recipe.autor))
+                }}>{recipe.autor}</a></p>
+                </div>
+                </a>
+              </div>
+            ))}
+          </div>
+      
+    </div>
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ IzvorniKod/UserFeed.jsx
@@ -0,0 +1,101 @@
+import React, {useEffect} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {fetchRecipesForUserFeed,fetchUserProfile} from "../../redux/auth/auth.action";
+
+const UserFeed = () => {
+    const jwtToken = localStorage.getItem("token");
+    const dispatch = useDispatch();
+    const recipesForFeed = useSelector(state => state.auth.recipesForFeed); // Adjust path according to your store structure
+    const loading = useSelector(state => state.auth.loading);
+    const error = useSelector(state => state.auth.error);
+    const userProfileInfo = useSelector(state => state.auth.userProfile);
+
+    useEffect(() => {
+        dispatch(fetchRecipesForUserFeed());
+    }, [dispatch]);
+
+    useEffect(() => {
+        dispatch(fetchUserProfile());
+      }, [dispatch]);
+
+
+    const korisnikRecepti = recipesForFeed.filter(recipe  =>
+       userProfileInfo.pratiteljiKorisnika.forEach(element => {
+           
+           return recipe.autor == element.korisnickoIme
+
+        })
+    );
+    const recipeCardContainerStyle = {
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexWrap: 'wrap',
+        
+      };
+    
+      const recipeCard = {
+        border: '1px solid #e0e0e0',
+        borderRadius: '8px',
+        overflow: 'hidden',
+        boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+        padding: '16px',
+        margin: '8px',
+        width: '30%', 
+        backgroundImage: 'url("/pexels-karolina-grabowska-4032977.jpg")',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        padding: '20px',
+        
+      };
+      
+      const textElementStyle = {
+        marginLeft: '90px', 
+      };
+
+    var arrayDataItems = korisnikRecepti.map(recipe => 
+            <div className="single-recipe-page">
+            <div className="recipe-card">
+            <a href="/RecipePage" onClick={()=>{localStorage.setItem('recipeToLoad',JSON.stringify(recipe.idRecept)); console.log(recipe.idRecept);}}>
+            <div className="recipe-details">
+                <h2>{recipe.nazivRecept}</h2>
+                {/*<p>Category: {recipe.category}</p>
+                <ul>
+                {recipe.ingredients.map((ingredient, index) => (
+                    <li key={index}>{ingredient}</li>
+                ))}
+                </ul>*/}
+                <p>Uputstva: {recipe.priprema}</p>
+            </div>
+            </a>
+            </div>
+        </div>
+    );
+    
+
+    return (
+        <div className="container mx-auto my-10 p-6 bg-white shadow-md rounded-md">
+            <h1 className="text-3xl font-bold mb-4 text-black">Welcome to Your User Feed!</h1>
+            {/*<p className="text-gray-600">Your JWT Token: {jwtToken}</p>*/}
+            <div className="container mx-auto my-10 p-6 bg-white shadow-md rounded-md">
+            <div style={recipeCardContainerStyle}>
+                {recipesForFeed.map(recipe => (
+                    <div key={recipe.idRecept} style={recipeCard }>
+                        <p style={textElementStyle}>{recipe.nazivRecept}</p>
+                        <p style={textElementStyle}>{recipe.autor}</p>
+                        <p style={textElementStyle}>{recipe.oznaka}</p>
+                        <p style={textElementStyle}>{recipe.vrijemeObjave}</p>
+                        
+                        {recipe.komentari.map(komentar => (
+                            <p style={textElementStyle}>{komentar.opisKomentar + " " + komentar.datumKomentar + " " + komentar.korisnik.korisnickoIme}</p>
+                        ))}
+                    </div>
+                ))}
+            </div>
+            {arrayDataItems}
+
+        </div></div>
+    );
+};
+
+export default UserFeed;
