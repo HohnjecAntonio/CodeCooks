b0b02455362e5074e525cd61ffba789c1a392a45
Armis Kantarević <ak53739@fer.hr>
2024-01-17 21:37:34 +0100

    Uređivanje stvari oko uređivanja Recepta

--- IzvorniKod/CookBooked/src/main/java/opp/CookBooked/service/implementacija/ReceptServiceJpa.java
+++ IzvorniKod/CookBooked/src/main/java/opp/CookBooked/service/implementacija/ReceptServiceJpa.java
@@ -244,30 +244,26 @@ public class ReceptServiceJpa implements ReceptService {
     }
 
     @Override
-    public Recept updateRecept(long idRecept, long idKorisnik, ReceptSubmitDTO updatedRecept) {
+    public Recept updateRecept(long idRecept, ReceptSubmitDTO updatedRecept) {
         try {
-            Korisnik k = korisnikService.findByIdKorisnik(idKorisnik);
+
+            Recept recept = receptRepo.findByIdRecept(idRecept);
 
             String sastojci = updatedRecept.getSastojci();
             String[] sastojciList = sastojci.split(",");
 
             List<String> sas = new ArrayList<>(List.of(sastojciList));
 
-            return receptRepo.findById(idRecept).map(recept -> {
-
                 recept.setNazivRecept(updatedRecept.getNazivRecept());
                 recept.setPriprema(updatedRecept.getPriprema());
-                recept.setAutor(k);
                 recept.setOznaka(updatedRecept.getOznaka());
                 recept.setSlikaRecept(updatedRecept.getSlikaRecept());
                 recept.setVideoRecept(updatedRecept.getVideoRecept());
                 recept.setVrijemeObjave(LocalDate.now());
                 recept.setVrijemeKuhanja(updatedRecept.getVrijemeKuhanja());
 
-
                 recSasService.dodajSastojkeReceptu(updatedRecept.getIdRecept(), sas);
                 return receptRepo.save(recept);
-            }).orElseThrow(() -> new RuntimeException("Recept not found with id " + idRecept));
         } catch (Exception e) {
             e.printStackTrace();
             return null;
