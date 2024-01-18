37069b49b4df6e4ee20d498f0049563c60503f9e
purham01 <benjamin.zrakic@fer.hr>
2024-01-18 22:57:20 +0100

    Update RecipePage.js

--- IzvorniKod/cookbookedfrontend/src/komponente/RecipePage.js
+++ IzvorniKod/cookbookedfrontend/src/komponente/RecipePage.js
@@ -293,7 +293,7 @@ const saveRecipeFunction = async (idKorisnik, idRecept) => {
             }}>{recipe.autor}</a></p>
               <p>Vrijeme kuhanja: {recipe.vrijemeKuhanja}</p>
               {
-                  recipe.kategorije[0] ?
+                  recipe.kategorije && recipe.kategorije.length > 0 ?
                   (
                     <p>Kategorija: {recipe.kategorije[0].nazivKategorija}</p>
                   )
@@ -305,7 +305,7 @@ const saveRecipeFunction = async (idKorisnik, idRecept) => {
 
                 
                 {
-                  recipe.vrsteKuhinje[0] ?
+                  recipe.vrsteKuhinje && recipe.vrsteKuhinje.length > 0 ?
                   (
                     <p>Vrste kuhinje: {recipe.vrsteKuhinje[0].nazivVrstaKuhinje}</p>
                   )
