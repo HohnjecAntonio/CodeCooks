08999e9b76565762ec2868cf0d285ceb80e45961
Antonio Hohnjec <148785817+HohnjecAntonio@users.noreply.github.com>
2024-01-19 17:43:10 +0100

    Update HomePage.jsx

--- IzvorniKod/cookbookedfrontend/src/pages/HomePage/HomePage.jsx
+++ IzvorniKod/cookbookedfrontend/src/pages/HomePage/HomePage.jsx
@@ -44,7 +44,7 @@ const HomePage = () => {
 
 function delayForDemo(promise) {
     return new Promise(resolve => {
-        setTimeout(resolve, 100);
+        setTimeout(resolve, 500);
     }).then(() => promise);
 }
 
